981b255084ae19a424a371a17dd2980d96dd11bf
Vanessa Oliveira <111035540+vanessa190290@users.noreply.github.com>
2023-03-14 20:44:14 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Produto {
+	// Atributos da classe,getters e setters 
+@Builder.Constructor
+Produto(long id, string titulo, string descricao, string marca 
+//Código do construtor
+}
+
+public class Main {
+	public static void main (string [] args){
+Produto produto=
+ new ProdutoBuilder()
+ titulo("MOUSE")
+ categoria ("Eletronicos")
+ estoque ("50")
+ preco ("20,00")
+ urlfoto ("/mouse.jpg")
+ build();
