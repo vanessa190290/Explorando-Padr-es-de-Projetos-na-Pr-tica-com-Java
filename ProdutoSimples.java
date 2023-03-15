9d7c64011c2f2d904c38b2843f187c642ac4b9b3
Vanessa Oliveira <111035540+vanessa190290@users.noreply.github.com>
2023-03-14 21:02:43 -0300

    Produto
    Produto

---
+++ ProdutoSimples.java
@@ -0,0 +1,15 @@
+public class;
+  class CafeDecorator Produto implements Cafe{
+
+	@Override
+	public double informarPreco() {
+		return 1;
+	}
+
+	@Override
+	public String informar Produto() {
+		return " Produto";
+	}
+	
+
+}
